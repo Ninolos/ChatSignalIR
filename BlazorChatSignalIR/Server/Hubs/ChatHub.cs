1df62f0d4776391b8891f392676eae02c56d3c67
Jonathan Daniel Loth <ninoloth@gmail.com>
2022-03-01 19:27:40 -0300

    Adding Usernames
    Show the Username on Connect & on Disconnect

--- BlazorChatSignalIR/Server/Hubs/ChatHub.cs
+++ BlazorChatSignalIR/Server/Hubs/ChatHub.cs
@@ -4,10 +4,20 @@ namespace BlazorChatSignalIR.Server.Hubs
 {
     public class ChatHub : Hub
     {
+        private static Dictionary<string, string> Users = new Dictionary<string, string>();
+
         public override async Task OnConnectedAsync()
         {
-            await AddMessageToChat("", "User connected!");
+            string username = Context.GetHttpContext().Request.Query["username"];
+            Users.Add(Context.ConnectionId, username);
+            await AddMessageToChat(string.Empty, $"{username} joined the party!");
             await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            string username = Users.FirstOrDefault(u => u.Key == Context.ConnectionId).Value;
+            await AddMessageToChat(string.Empty, $"{username} left!");
         }
 
         public async Task AddMessageToChat(string user, string message)
